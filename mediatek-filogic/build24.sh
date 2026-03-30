bad16e2589202ce72aab6b27eb9a0aaa938ce863
cenji1111 <88656357+cenji1111@users.noreply.github.com>
2026-03-30 16:27:54 +0800

    Update build24.sh

--- mediatek-filogic/build24.sh
+++ mediatek-filogic/build24.sh
@@ -59,7 +59,7 @@ PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
 # 文件管理器
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
-PACKAGES="$PACKAGES luci-app-openclash luci-i18n-openclash-zh-cn"
+PACKAGES="$PACKAGES luci-app-openclash"
 PACKAGES="$PACKAGES xray-core sing-box"
 
 
